51617642a32655cf00a550c4e9f6df4007b4ddb0
Thomas Persbråten <83210455+Thomaspersbraaten@users.noreply.github.com>
2021-09-28 21:39:28 +0200

    js starting

---
+++ js/script1.js
@@ -0,0 +1,37 @@
+
+
+
+// Week delivery
+
+// Question 1 
+
+var food = "lobster";
+var price = 10.99;
+var isAlive = true;
+
+console.log(food,price,isAlive);
+
+// Or
+
+console.log(" ");
+
+console.log("Type of food:", food);
+console.log("price:", price);
+console.log("food is living:", isAlive);
+
+// second question 
+console.log(" ");
+
+var firstName="Thomas";
+var lastName="Persbråten";
+var fullName= firstName + " " + lastName;
+
+console.log(fullName);
+
+// Question 3
+
+var animal1 = "frog"
+
+console.log("the type of frog is", typeof animal1);
+
+
---
+++ js/script2.js
@@ -0,0 +1,108 @@
+// console.log("This is script 2")
+
+var firstName = "mary";
+var nameContainer = document.querySelector("#name");
+
+console.log(nameContainer);
+
+nameContainer.innerHTML = firstName;
+
+console.log("This is script 1")
+
+ var pet = "dog";
+ console.log(pet)
+
+//  var letters = "a" + "b"
+
+//  console.log(letters);
+
+//  var letter1 = "a"
+//  var letter2 = "b"
+
+// var letters = letter1 + letter2;
+
+// console.log(letters)
+
+// yosha
+
+
+console.log("q1");
+
+
+var number1 = 5;
+var number2 = 5.6;
+
+console.log("q2");
+var week = "monday" + " " + "tuesday" + " " + "wednesday" + " " + "Thursday" + " " + "friday" + " " + "saturday" + " " + "sunday"
+console.log(week);
+
+
+
+console.log("q3");
+
+var addition = number1 + number2;
+console.log(addition);
+var substraction = number1 - number2;
+console.log(substraction);
+var multiplication = number1 * number2;
+console.log(multiplication);
+var division = number1 / number2;
+console.log(division);
+
+console.log("q4");
+
+var walking = false;
+console.log(walking);
+
+console.log("q5");
+var characters = "the frogs are angry";
+// var lengthCharacters = characters.length;
+// console.log(lengthCharacters);
+console.log(characters.length);
+
+console.log("Q6");
+
+var time_elapsed = 1;
+var totalprice = 1;
+
+var timeElapsed = time_elapsed;
+var totalPrice = totalprice;
+
+console.log(totalPrice, time_elapsed);
+
+console.log("Q7");
+
+var number1 = "53";
+var number2 = "44.6";
+
+var numbervalue1 = parseInt(number1);  
+var numbervalue2 = parseInt(number2);
+
+console.log(numbervalue1 + numbervalue2);
+
+console.log("Q8");
+
+var pet = "donkey";
+var age = 89;
+var discountApplied = true;
+var timeLeft;
+
+console.log(typeof pet);
+console.log(typeof age);
+console.log(typeof discountApplied);
+console.log(typeof timeLeft);
+
+console.log("Q9");
+
+var updateHeading = "Updating heading"
+var headercontainer = document.querySelector("#main-header");
+
+headercontainer.innerHTML = updateHeading;
+headercontainer.style.margin = "50px auto"
+
+console.log("Q10");
+
+var body = document.querySelector("body");
+body.style.backgroundColor = "green";
+body.style.opacity = "50%";
+body.style.textAlign = "center";
